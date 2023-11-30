1cd0110ecb00f2160214592c3d468880e89117e8
GokulRajMobiledev <151429788+GokulRajMobiledev@users.noreply.github.com>
2023-12-01 00:24:30 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './sass/style.scss'
+import '../src/main.scss'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
