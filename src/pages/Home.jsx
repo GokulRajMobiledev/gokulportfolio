7b0ed1f2c08292873f50a3adc026cdd08529f7ae
GokulRajMobiledev <151429788+GokulRajMobiledev@users.noreply.github.com>
2023-12-01 01:16:02 +0530

    removed data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,44 @@
-import data from '../HomePageData.json';
-import Hero from '../components/Hero/Hero';
-import About from '../components/About/About';
-import Iconbox from '../components/Iconbox/Iconbox';
-import Skill from '../components/Skill/Skill';
-import Resume from '../components/Resume/ResumeSection';
+import data from "../HomePageData.json";
+import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
+import Iconbox from "../components/Iconbox/Iconbox";
+import Skill from "../components/Skill/Skill";
+import Resume from "../components/Resume/ResumeSection";
 import Contact from "../components/Contact/Contact";
-import PortfolioSection from '../components/Protfolio/PortfolioSection';
+import PortfolioSection from "../components/Protfolio/PortfolioSection";
 
 const Home = () => {
-  const { heroData, aboutData, serviceData, animated, skillData, portfolioData, blogData, resumeData, reviewData, contactData, socialData } = data;
+  const {
+    heroData,
+    aboutData,
+    serviceData,
+    animated,
+    skillData,
+    portfolioData,
+    resumeData,
+    contactData,
+    socialData,
+  } = data;
   return (
     <>
-      <Hero data={heroData} animated={animated} socialData={socialData} data-aos="fade-right" />
+      <Hero
+        data={heroData}
+        animated={animated}
+        socialData={socialData}
+        data-aos="fade-right"
+      />
       <About data={aboutData} data-aos="fade-right" />
       <Iconbox data={serviceData} data-aos="fade-right" />
       <Skill data={skillData} data-aos="fade-right" />
       <Resume data={resumeData} />
       <PortfolioSection data={portfolioData} data-aos="fade-right" />
-      <Contact data={contactData} socialData={socialData} data-aos="fade-right" />
+      <Contact
+        data={contactData}
+        socialData={socialData}
+        data-aos="fade-right"
+      />
     </>
-  )
-}
-
+  );
+};
 
 export default Home;
