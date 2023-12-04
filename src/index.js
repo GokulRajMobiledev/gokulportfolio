5d53b2db86a0bca6cb78684ff44c915040f4f158
GokulRajMobiledev <151429788+GokulRajMobiledev@users.noreply.github.com>
2023-12-04 18:12:27 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/style.scss'
-import '../src/main.css'
+import '../src/index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
